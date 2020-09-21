df7613a9f6986796dcc11d8b3b57d9051a924894
arnaudsm <arnaudsm66@gmail.com>
2020-09-21 12:50:39 +0200

    Refactoring

--- bigot.py
+++ bigot.py
@@ -2,7 +2,7 @@ import collections
 import functools
 import math
 import operator
-import timeit
+from timeit import Timer
 from math import log
 from time import time
 
@@ -10,6 +10,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from memory_profiler import memory_usage
+from pprint import pprint
 
 complex_funcs = {
     "O(n)": lambda n, avg_n, avg_t: n*avg_t/avg_n,
@@ -24,24 +25,27 @@ complex_funcs = {
 }
 
 
-def calculate_complexity(data, function, col, plot=False, filter_values=False):
-    data = data[[col, "n"]].dropna(subset=[col])
-    if filter_values:
-        window_size = int(data.shape[0]/20)+1
-        data = data.rolling(window_size).min().dropna(subset=[col])
-    print(1)
+def calculate_complexity(data, function, col, bench_func, plot=False):
+    data = data[[col, "n"]].dropna(subset=[col]).groupby(
+        ["n"]).median().reset_index()
+    data[col] = data[col]-data[col].min()
+
+    # Filtering
     print(data)
-    print(data.shape)
-    print(data.quantile([.5, 1]).median())
+    if False:
+        window_size = int(data.shape[0]/20)+2
+        data_raw = data.copy()
+        if col == "t":
+            data = data.rolling(window_size).min().dropna(subset=[col])
+        elif col == "m":
+            data = data.rolling(window_size).max().dropna(subset=[col])
+
     assert data.shape[0] > 0
     last_chunk = data.quantile([.5, 1]).median()
-    print(1.5)
     first_chunk = data.quantile([0, .5]).median()
-    print(2)
     x = list(data.n.unique())
     avg_n = data.n.median()
     avg_t = data[col].median()
-    print(5)
 
     losses = {}
     for func_name, func in complex_funcs.items():
@@ -57,23 +61,35 @@ def calculate_complexity(data, function, col, plot=False, filter_values=False):
     # Hesitation check
     losses_further = {}
     n_further = int(data.n.max() * 2)
+    pprint(losses)
     for func_name, loss in losses.items():
-        if loss < 2:
+        if loss < 4:
             func = complex_funcs[func_name]
             t_predicted_further = func(n_further, avg_n, avg_t)
             losses_further[func_name] = t_predicted_further
 
     if len(losses_further) > 1:
-        t_expected_further = timeit.Timer(functools.partial(function, n_further)).timeit(10)/10*1000  # noqa
-        losses_further = {name: abs(t_further/t_expected_further) for (name, t_further) in losses_further.items()}  # noqa
+        print("Hesitation", func_closest)
+
+        col_expected_further = bench_func(n_further)  # noqa
+        data = data.append({
+            "n": n_further,
+            col: col_expected_further,
+        }, ignore_index=True)
+        losses_further = {name: abs(col_further/col_expected_further) for (name, col_further) in losses_further.items()}  # noqa
         func_closest = min(losses_further, key=losses_further.get)
+        print("Hesitation", func_closest)
+        pprint(losses_further)
 
     if plot:
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=data.n, y=data[col], name="Observation",
                                  line=dict(width=1)))
+        # fig.add_trace(go.Scatter(x=data_raw.n, y=data_raw[col], name="RAW",
+        #                         line=dict(width=1)))
 
         for func_name, func in complex_funcs.items():
+            x = list(data.n.unique())
             y = [func(n, avg_n, avg_t) for n in x]
             if max(y) < 2*data[col].max():
                 fig.add_trace(go.Scatter(x=x, y=y, name=func_name,
@@ -84,15 +100,16 @@ def calculate_complexity(data, function, col, plot=False, filter_values=False):
     return func_closest
 
 
-def benchmark(function, time_budget=1000, memory_budget=1000, plot=False):
+def benchmark(function, time_budget=1000, memory=False, plot=False):
     data = pd.DataFrame()
 
     # Time check
     test_time = time()
-    time_function = lambda n: timeit.Timer(functools.partial(function, n)).timeit(3)/3*1000
-    min_t = timeit.Timer(functools.partial(function, 1)).timeit(10)/10*1000
+
+    def func_t(n, runs=1): return Timer(functools.partial(
+        function, int(n))).timeit(runs)/runs*1000
     for n in [n for n in range(1, 10000)]:
-        t = timeit.Timer(functools.partial(function, n)).timeit(3)/3*1000 - min_t
+        t = func_t(n)
         data = data.append({
             "n": int(n),
             "t": t
@@ -101,15 +118,15 @@ def benchmark(function, time_budget=1000, memory_budget=1000, plot=False):
             break
     if data.shape[0] > 10000:
         data = data.sample(n=10000)
+    time_complexity = calculate_complexity(data, function, "t", func_t, plot=plot)  # noqa
+    print("Time complexity:", time_complexity)
+
 
     # Memory check
-    min_m = max(memory_usage(proc=(function, [data.iloc[0].n]), interval=(data.iloc[0].t/1000000)))  # noqa
-    for index, row in data.sample(n=10).iterrows():
-        data.loc[index, "m"] = max(memory_usage(proc=(function, [row.n]), interval=(row.t/1000000))) - min_m  # noqa
-
-    data = data.groupby(["n"]).median().reset_index().sort_values(by="n")
-
-    time_complexity = calculate_complexity(data, function, "t", filter_values=True)  # noqa
-    print("Time complexity:", time_complexity)
-    space_complexity = calculate_complexity(data, function, "m", plot=True)  # noqa
-    print("Space complexity:", space_complexity)
+    if memory:
+        def func_m(n): return max(memory_usage(
+            proc=(function, [int(n)]), interval=(0.01)))
+        for index, row in data.iterrows():
+            data.loc[index, "m"] = func_m(row.n)
+        space_complexity = calculate_complexity(data, function, "m", func_m, plot=plot)  # noqa
+        print("Space complexity:", space_complexity)
