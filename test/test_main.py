0f0c48952283634a92402a381329304779055514
arnaudsm <arnaudsm66@gmail.com>
2020-09-24 00:31:49 +0200

    Updated tests for new classes

--- test/test_main.py
+++ test/test_main.py
@@ -1,27 +1,41 @@
-import time
+from time import sleep
 from math import factorial, log
 
 import pytest
 
-from bigot import Bigot, complex_funcs
+from bigot import Time, Space, Compare, complex_funcs
 
 
 def test_time_benchmark():
     def fake_function(n, func):
-        time.sleep(0.01*func(n, 2, 2))
+        sleep(0.01*func(n, 2, 2))
 
     for name, real_function in complex_funcs.items():
         def function(n): return fake_function(n, real_function)
         print("Checking time complexity of", name)
-        assert name == Bigot(function, name=name).time()
+        assert name == Time(function, name=name).time()
 
 
 def test_space_benchmark():
     def fake_function(n, func):
         x = 10000000*"-"*int(func(n, 2, 2))
-        time.sleep(0.01)
+        sleep(0.01)
 
     for name, real_function in complex_funcs.items():
         def function(n): return fake_function(n, real_function)
         print("Checking space complexity of", name)
-        assert name == Bigot(function, name=name).space()
+        assert name == Space(function, name=name).space()
+
+
+def test_space_benchmark():
+    def on(n):
+        x = 10000000*"-"*int(n)
+        sleep(0.001*n)
+
+    def on2(n):
+        x = 10000000*"-"*int(n**2)
+        sleep(0.001*n**2)
+
+    assert Compare([on, on2]).all().shape == (2, 7)
+    assert Compare([on, on2]).time().shape == (2, 4)
+    assert Compare([on, on2]).space().shape == (2, 4)
