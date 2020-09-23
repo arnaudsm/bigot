9670751b760cb2a1245a04a7b4010e025946e542
arnaudsm <arnaudsm66@gmail.com>
2020-09-24 01:11:29 +0200

    Added long_description and url in README

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 """Setup."""
 from setuptools import setup, find_packages
 
+with open("README.MD", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="bigot",
     version="1.0.0",
@@ -13,6 +16,9 @@ setup(
     ],
     author="Arnaud de Saint Meloir",
     author_email="arnaud.desaintmeloir@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arnaudsm/bigot",
     description="Benchmarking library with Space and Time Complexity estimation",
     include_package_data=True
 )
